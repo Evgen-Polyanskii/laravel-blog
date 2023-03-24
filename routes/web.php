dfd85d7428707bd3e45eb5f9f0c754a8a770ad94
Evgeniy <evgen-polyanskii@yandex.ru>
2023-03-24 11:56:41 +0300

    refact: changed the about page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
-Route::get('about', function () {
-    return view('about');
-});
+Route::view('/about', 'about');
