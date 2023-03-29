f26b84f061a44ebacaae90d6f46965c55e38ca77
Evgeniy <evgen-polyanskii@yandex.ru>
2023-03-29 20:29:02 +0300

    refact: changed entity Article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,68 +2,83 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\StoreArticleRequest;
+use App\Http\Requests\UpdateArticleRequest;
 use App\Models\Article;
 
 class ArticleController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         $articles = Article::paginate(2);
-
+        
         return view('article.index', compact('articles'));
     }
 
-    public function show($id)
-    {
-        $article = Article::findOrFail($id);
-        return view('article.show', compact('article'));
-    }
-
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         $article = new Article();
         return view('article.create', compact('article'));
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(StoreArticleRequest $request)
     {
-        $article = new Article();
         $data = $request->input();
+        $article = new Article();
 
         $article->fill($data);
         $article->save();
-        
+
         $request->session()->flash('success', 'Article added!');
         return redirect()->route('articles.index');
     }
 
-    public function edit($id)
+    /**
+     * Display the specified resource.
+     */
+    public function show(Article $article)
     {
-        $article = Article::findOrFail($id);
+        return view('article.show', compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Article $article)
+    {
         return view('article.edit', compact('article'));
     }
 
-    public function update(StoreArticleRequest $request, $id)
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateArticleRequest $request, Article $article)
     {
-        $article = Article::findOrFail($id);       
         $data = $request->input();
-        
+
         $article->fill($data);
         $article->save();
-        
+
         $request->session()->flash('success', 'Article has been updated!');
         return redirect()->route('articles.index');
     }
 
-    public function destroy(Request $request, $id)
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
     {
-        $article = Article::find($id);
-        if ($article) {
-            $article->delete();
-        }
-        
+        $article->delete();
+
         $request->session()->flash('success', 'Article has been deleted!');
         return redirect()->route('articles.index');
     }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -4,14 +4,15 @@
 
 @section('content')
     @foreach($articles as $article)
-        <a href="{{ route('articles.show', [ 'id' => $article->id ]) }}"><h2>{{$article->name}}</h2></a>
+        <a href="{{ route('articles.show', $article) }}"><h2>{{$article->name}}</h2></a>
         <div>{{Str::limit($article->body, 200)}}</div>
         <div>
-            <a href="{{ route('articles.edit', [ 'id' => $article->id ]) }}">Изменить</a>
+            <a href="{{ route('articles.edit', $article) }}">Изменить</a>
             <a 
-                href="{{ route('articles.destroy', [ 'id' => $article->id ]) }}" 
+                href="{{ route('articles.destroy', $article) }}" 
                 rel="nofollow"
                 data-method="delete"
+                data-confirm="Вы уверены?"
             >Удалить</a>
         </div>
     @endforeach
